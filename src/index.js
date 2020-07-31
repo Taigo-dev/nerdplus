7a43b4e3c143e9614b2804a4b2079943181bcee5
Tiago Souza <tiago.souza@zup.com.br>
2020-07-31 18:24:47 -0300

    Comitar projeto

--- src/index.js
+++ src/index.js
@@ -1,22 +1,21 @@
-import React from "react";
+import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import Home from './pages/Home';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import Home from './pages/Home';
 import CadastroVideo from './pages/cadastro/Video';
 import CadastroCategoria from './pages/cadastro/Categoria';
 
-
-const pagina404 = () => (<h1>Página 404</h1>)
+const pagina404 = () => (<h1>Página 404</h1>);
 
 ReactDOM.render(
   <BrowserRouter>
     <Switch>
       <Route exact path="/" component={Home} />
-      <Route exact path="/cadastro/video" component={CadastroVideo}/>
-      <Route exact path="/cadastro/categoria" component={CadastroCategoria}/> 
-      <Route component={pagina404}/>
+      <Route exact path="/cadastro/video" component={CadastroVideo} />
+      <Route exact path="/cadastro/categoria" component={CadastroCategoria} />
+      <Route component={pagina404} />
     </Switch>
   </BrowserRouter>,
-  document.getElementById("root")
+  document.getElementById('root'),
 );
--- src/pages/cadastro/Categoria/index.js
+++ src/pages/cadastro/Categoria/index.js
@@ -1,20 +1,117 @@
-import React from "react";
-import PageDefault from "../../../components/PageDefault";
-import { Link } from "react-router-dom";
+import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
+import PageDefault from '../../../components/PageDefault';
+import FormField from '../../../components/FormField';
+import Button from '../../../components/Button';
 
 function CadastroVideo() {
+  const valoresIniciais = {
+    nome: '',
+    descricao: '',
+    cor: '',
+  };
+
+  const [categorias, setCategorias] = useState([]);
+  const [values, setValues] = useState(valoresIniciais);
+
+  function setValue(chave, valor) {
+    setValues({
+      ...values,
+      [chave]: valor,
+    });
+  }
+
+  function handleChange(infosDoEvento) {
+    setValue(
+      infosDoEvento.target.getAttribute('name'),
+      infosDoEvento.target.value,
+    );
+  }
+  // function handleChange(infosDoEvento) {
+  //   const { getAttribute, value } = infosDoEvento.target;
+  //   setValue(
+  //     getAttribute('name'),
+  //     value
+  //   );
+  // }
+
+  useEffect(() => {
+    const url = 'http://localhost:8080/categorias';
+    fetch(url).then(async (respotaDoServidor) => {
+      const resposta = await respotaDoServidor.json();
+      setCategorias([
+        ...resposta,
+      ]);
+    });
+    // setTimeout(() => {
+    //   setCategorias([
+    //     ...categorias,
+    //     {
+    //       id: 1,
+    //       nome: 'Front End',
+    //       descricao: 'Infos da categoria',
+    //       cor: '#cbd1ff',
+    //     },
+    //     {
+    //       id: 1,
+    //       nome: 'Back End',
+    //       descricao: 'Infos da categoria',
+    //       cor: '#cbd1ff',
+    //     },
+    //   ]);
+    // }, 4 * 1000);
+  }, []);
+
   return (
     <PageDefault>
-      <h1>Cadastro de categoria</h1>
-      <form>
-        <label>
-          Nome da categoria:
-          <input type="text" />
-        </label>
-        <button>
-          Cadastrar
-        </button>
+      <h1>
+        Cadastro de categoria:
+        {values.nome}
+      </h1>
+      <form
+        // style={{ background: values }}
+        onSubmit={function handleSubmit(infosDoEvento) {
+          infosDoEvento.preventDefault();
+          setCategorias([...categorias, values]);
+          setValues(valoresIniciais);
+        }}
+      >
+        <FormField
+          label="Nome da categotia "
+          type="text"
+          name="nome"
+          value={values.nome}
+          onChange={handleChange}
+        />
+
+        <FormField
+          label="Descrição"
+          type="textarea"
+          name="descricao"
+          value={values.descricao}
+          onChange={handleChange}
+        />
+        <FormField
+          label="Cor "
+          type="color"
+          name="cor"
+          value={values.cor}
+          onChange={handleChange}
+        />
+        <Button>Cadastrar</Button>
       </form>
+
+      {categorias.length === 0 && (
+        <div>
+          Loading
+        </div>
+      )}
+
+      <ul>
+        {categorias.map((categoria, indice) => (
+          <li key={`${categoria}${indice}`}>{categoria.nome}</li>
+        ))}
+      </ul>
       <Link to="/">Ir para a home</Link>
     </PageDefault>
   );
